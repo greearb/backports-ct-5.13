45bad6d07e814d9120158d024cbf2f35aa6fbc2a
Ben Greear <greearb@candelatech.com>
2021-07-02 07:38:38 -0700

    apply backport SmPL patch 0042-pci_error_handlers/igb_pci_error_handlers.cocci
    (took 2.92 seconds)

--- drivers/net/wireless/marvell/mwifiex/pcie.c
+++ drivers/net/wireless/marvell/mwifiex/pcie.c
@@ -559,7 +559,11 @@ static void mwifiex_pcie_reset_done(struct pci_dev *pdev)
 	card->pci_reset_ongoing = false;
 }
 
-static const struct pci_error_handlers mwifiex_pcie_err_handler = {
+static 
+#if LINUX_VERSION_IS_GEQ(3,7,0)
+const 
+#endif /* LINUX_VERSION_IS_GEQ(3,7,0) */
+struct pci_error_handlers mwifiex_pcie_err_handler = {
 	.reset_prepare		= mwifiex_pcie_reset_prepare,
 	.reset_done		= mwifiex_pcie_reset_done,
 };
