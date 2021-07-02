52ad83bad9b9908e16e1c9041f16a76f9c896594
Ben Greear <greearb@candelatech.com>
2021-07-02 07:37:57 -0700

    apply backport patch devcoredump.patch
    (took 0.05 seconds)

--- include/linux/backport-devcoredump.h
+++ include/linux/backport-devcoredump.h
@@ -51,7 +51,7 @@ static inline void _devcd_free_sgtable(struct scatterlist *table)
 }
 
 
-#ifdef CONFIG_DEV_COREDUMP
+#ifdef CPTCFG_BPAUTO_WANT_DEV_COREDUMP
 void dev_coredumpv(struct device *dev, void *data, size_t datalen,
 		   gfp_t gfp);
 
@@ -85,6 +85,6 @@ static inline void dev_coredumpsg(struct device *dev, struct scatterlist *table,
 {
 	_devcd_free_sgtable(table);
 }
-#endif /* CONFIG_DEV_COREDUMP */
+#endif /* CPTCFG_BPAUTO_WANT_DEV_COREDUMP */
 
 #endif /* __DEVCOREDUMP_H */
